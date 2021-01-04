e741e2413a1a822e3cf86bfab8ccc58b3642084e
Henrik Steen <henrist@henrist.net>
2021-01-04 14:16:10 +0100

    Fix error on new lambda version creation

--- packages/infrastructure/src/repo-metrics-edge-stack.ts
+++ packages/infrastructure/src/repo-metrics-edge-stack.ts
@@ -12,6 +12,7 @@ export class RepoMetricsEdgeStack extends cdk.Stack {
 
     this.authLambdas = new AuthLambdas(this, "AuthLambdas", {
       regions: ["eu-west-1"],
+      nonce: "2",
     })
   }
 }
