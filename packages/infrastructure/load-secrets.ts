7fbd7fd94b99274ce671a60acecb067448ad6bb3
Henrik Steen <henrist@henrist.net>
2021-06-14 08:00:24 +0200

    Migrate secrets script

---
+++ packages/infrastructure/load-secrets.ts
@@ -0,0 +1,50 @@
+import { loadSecrets } from "@capraconsulting/cals-cli"
+
+const githubTokenSecret: loadSecrets.Secret = {
+  name: "github-token",
+  description: "GitHub token",
+  type: "json",
+  fields: [
+    {
+      key: "token",
+    },
+  ],
+}
+
+const snykTokenSecret: loadSecrets.Secret = {
+  name: "snyk-token",
+  description: "Snyk token",
+  type: "json",
+  fields: [
+    {
+      key: "token",
+    },
+  ],
+}
+
+const reporterSlackWebhookUrlSecret: loadSecrets.Secret = {
+  name: "reporter-slack-webhook-url",
+  description: "Slack Webhook URL for data reporting",
+  type: "json",
+  fields: [
+    {
+      key: "url",
+    },
+  ],
+}
+
+loadSecrets.loadSecretsCli({
+  secretGroups: [
+    {
+      accountId: "001112238813",
+      region: "eu-west-1",
+      description: "incub",
+      namePrefix: "/incub/repo-metrics/",
+      secrets: [
+        githubTokenSecret,
+        snykTokenSecret,
+        reporterSlackWebhookUrlSecret,
+      ],
+    },
+  ],
+})
