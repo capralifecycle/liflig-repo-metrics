64df19064ba53c0d21b6e95e612d9b08852e9e93
evan-boone <ebo@capraconsulting.no>
2023-10-27 09:10:33 +0200

    fix: increase memory for aggregator function to 2560 MB

--- packages/infrastructure/src/repo-metrics-stack.ts
+++ packages/infrastructure/src/repo-metrics-stack.ts
@@ -163,6 +163,7 @@ export class RepoMetricsStack extends cdk.Stack {
       handler: "index.aggregateHandler",
       runtime: lambda.Runtime.NODEJS_16_X,
       timeout: cdk.Duration.minutes(5),
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_229_0,
       memorySize: 2560,
       environment: {
         DATA_BUCKET_NAME: dataBucket.bucketName,
