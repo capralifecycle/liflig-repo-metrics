0ec47062769954d3e63cac1d52f2419d36878356
Joakim L. Engeset <joakim.engeset@gmail.com>
2025-12-18 14:00:13 +0100

    feat: add tiny cachedir implementation

--- packages/repo-collector/src/config.ts
+++ packages/repo-collector/src/config.ts
@@ -1,11 +1,38 @@
 import https from "node:https"
+import os from "node:os"
 import path from "node:path"
-import cachedir from "cachedir"
 
 export class Config {
   public cwd = path.resolve(process.cwd())
-  public cacheDir = cachedir("cals-cli")
+  public cacheDir = path.join(getOsCacheDir(), "repo-collector")
   public agent = new https.Agent({
     keepAlive: true,
   })
+}
+
+function getOsCacheDir(): string {
+  const cacheDirs = {
+    posix: () => {
+      return process.env.XDG_CACHE_HOME || path.join(os.homedir(), ".cache")
+    },
+    darwin: () => {
+      return path.join(os.homedir(), "Library", "Caches")
+    },
+    win32: () => {
+      const appData =
+        process.env.LOCALAPPDATA || path.join(os.homedir(), "AppData", "Local")
+      return path.join(appData, "Cache")
+    },
+  } as const
+
+  switch (os.platform()) {
+    case "darwin":
+      return cacheDirs.darwin()
+    case "win32":
+      return cacheDirs.win32()
+    case "linux":
+      return cacheDirs.posix()
+    default:
+      throw new Error(`Unsupported OS platform: ${os.platform()}`)
+  }
 }
