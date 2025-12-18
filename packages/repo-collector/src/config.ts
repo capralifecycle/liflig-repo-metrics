6285d2756a8ab356bad41537c9964189e73f7603
Joakim L. Engeset <joakim.engeset@gmail.com>
2025-12-18 10:56:47 +0100

    fix: re-add old cache dep
    we didn't resolve the expected posix path, as stepfunction fails on
    creation of cache dir.

--- packages/repo-collector/src/config.ts
+++ packages/repo-collector/src/config.ts
@@ -1,16 +1,10 @@
 import https from "node:https"
-import { homedir, platform } from "node:os"
 import path from "node:path"
+import cachedir from "cachedir"
 
 export class Config {
   public cwd = path.resolve(process.cwd())
-  public cacheDir = (() => {
-    const baseDir =
-      platform() === "darwin"
-        ? path.join(homedir(), "Library/Caches")
-        : path.join(homedir(), ".cache")
-    return path.join(baseDir, "cals-cli")
-  })()
+  public cacheDir = cachedir("cals-cli")
   public agent = new https.Agent({
     keepAlive: true,
   })
